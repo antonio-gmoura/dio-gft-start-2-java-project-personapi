9c7b1312abffb46887e4f139a6608c1d4db3e8cc
Antonio Moura <antonio.gmoura@gmail.com>
2021-07-22 01:28:46 -0400

    Developed Person Created Operation

--- src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -6,12 +6,29 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import one.digitalinnovation.personapi.enums.PhoneType;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
     private PhoneType type;
+
+    @Column(nullable = false, length = 20)
     private String number;
 }
